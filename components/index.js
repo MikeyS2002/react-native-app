a7956ecea4c1c92bb3fa82c31c424ab83b88d637
Mike Schaper <mikeschaper12@gmail.com>
2022-04-02 11:24:39 +0200

    inital

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NftCard from "./NftCard";
+import { CircleButton, RectButton} from "./Button";
+import SubInfo from "./SubInfo";
+
+export {
+    HomeHeader, NftCard, FocusedStatusBar, CircleButton, SubInfo
+}
